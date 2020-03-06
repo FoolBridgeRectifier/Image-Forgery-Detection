ec41280bc9f4faca0ab58ebe52b42888ef15f9f8
Sathyanarayanan Somasunder <37745801+FoolBridgeRectifier@users.noreply.github.com>
2020-03-06 11:17:43 -0500

    Update code.m

--- code.m
+++ code.m
@@ -1,129 +1,181 @@
-imgSize=512;
-maskSize=50; % change for mask size
-img = imread("img.png");
-mask=imread("mask.jpg");
-%format mask and input image
-mask=imbinarize(rgb2gray(mask),0.6);
-mask=imresize(mask,[maskSize,maskSize]);
-img=imresize(img,[imgSize,imgSize]);
-img=rgb2gray(img);
-imgin=img;
-imgPossible=zeros([imgSize,imgSize,8]);
-key=[4,3,2]; % key for which layers to encode watermark
+img = imread("books-8.jpg");
  
-simVal=1;
-bits=max(key);
-%Check positions where encoding is possible
-for i=1:imgSize
-    disp(i)
-   for j=1:imgSize
-       % Cycle through each layer
-       for b=1:bits
-           val=extractBit(img,b,[i,j]);
-           flag=0;
-           % if rest of bits are opposit to considered bit
-           for n = simVal:(b-1)
-               if(val==extractBit(img,n,[i,j]))
-                   flag=1;
-                   break
-               end
-           end
-           if(flag==0)
-               img=addToBit(img,b,[i,j],0);
-               for n = simVal:(b-1)
-                    img=addToBit(img,n,[i,j],1);
-               end
-               imgPossible(i,j,b)=1;
-           end
-       end
-   end
-end
-% add the mask to image
-for i=1:imgSize
-    disp(i)
-    for j=1:imgSize
-        m=mod(i,maskSize)+1;
-        n=mod(j,maskSize)+1;
-        b=key(mod((i*imgSize)+j,length(key))+1);
-        if(imgPossible(i,j,b)==1)
-            img=addToBit(img,b,[i,j],mask(m,n));
-            for k = simVal:(b-1)
-                if(k==0&&k+n<maskSize)
-                    % little variation for JPEG compression
-                    img=addToBit(img,k,[i,j],1-mask(m,k+n));
+%limiting size to 250 to reduce processing time
+size=250;
+img=imresize(img,[size,size]);
+ 
+s=15;
+d=10;
+imggray=rgb2gray(img);
+imgx=zeros([size,size]);
+imgy=zeros([size,size]);
+ii=size;
+jj=size;
+disp("1");
+% creating strength matrices in x and y direction
+for i = 1:size
+    jj=size;
+    for j=1:size
+        xp=0;
+        xn=0;
+        yp=0;
+        yn=0;
+        flagxp=1;
+        flagxn=1;
+        flagyp=1;
+        flagyn=1;
+        while(flagxp ||flagyp ||flagxn ||flagyn )
+            if(xp+i<size&&flagxp)
+                a1=abs(abs(img(xp+i,j,1)-img(xp+i+1,j,1))-abs(img(xp+i,j,2)-img(xp+i+1,j,2)))<s;
+                a2=abs(abs(img(xp+i,j,1)-img(xp+i+1,j,1))-abs(img(xp+i,j,3)-img(xp+i+1,j,3)))<s;
+                a3=abs(abs(img(xp+i,j,3)-img(xp+i+1,j,3))-abs(img(xp+i,j,2)-img(xp+i+1,j,2)))<s;
+                b1=abs(img(xp+i,j,1)-img(xp+i+1,j,1))>d;
+                b2=abs(img(xp+i,j,2)-img(xp+i+1,j,2))>d;
+                b3=abs(img(xp+i,j,3)-img(xp+i+1,j,3))>d;
+                if(a1&&a2&&a3&&b1&&b2&&b3&&flagxp==1)
+                    flagxp=2;
+                    xp=xp+1;
+                elseif(a1&&a2&&a3&&flagxp==2)
+                    xp=xp+1;
                 else
-                    img=addToBit(img,k,[i,j],1-mask(m,k));
+                    flagxp=0;
                 end
-                
+            else
+                flagxp=0;
+            end
+            
+            if(yp+j<size&&flagyp)
+                a1=abs(abs(img(i,yp+j,1)-img(i,yp+j+1,1))-abs(img(i,yp+j,2)-img(i,yp+j+1,2)))<s;
+                a2=abs(abs(img(i,yp+j,1)-img(i,yp+j+1,1))-abs(img(i,yp+j,3)-img(i,yp+j+1,3)))<s;
+                a3=abs(abs(img(i,yp+j,3)-img(i,yp+j+1,3))-abs(img(i,yp+j,2)-img(i,yp+j+1,2)))<s;
+                b1=abs(img(i,yp+j,1)-img(i,yp+j+1,1))>d;
+                b2=abs(img(i,yp+j,2)-img(i,yp+j+1,2))>d;
+                b3=abs(img(i,yp+j,3)-img(i,yp+j+1,3))>d;
+                if(a1&&a2&&a3&&b1&&b2&&b3&&flagyp==1)
+                    flagyp=2;
+                    yp=yp+1;
+                elseif(a1&&a2&&a3&&flagyp==2)
+                    yp=yp+1;
+                else
+                    flagyp=0;
+                end
+            else
+                flagyp=0;
+            end
+            
+          
+            if(xn+ii>1&&flagxn)
+                a1=abs(abs(img(xn+ii,j,1)-img(xn+ii-1,j,1))-abs(img(xn+ii,j,2)-img(xn+ii-1,j,2)))<s;
+                a2=abs(abs(img(xn+ii,j,1)-img(xn+ii-1,j,1))-abs(img(xn+ii,j,3)-img(xn+ii-1,j,3)))<s;
+                a3=abs(abs(img(xn+ii,j,3)-img(xn+ii-1,j,3))-abs(img(xn+ii,j,2)-img(xn+ii-1,j,2)))<s;
+                b1=abs(img(xn+ii,j,1)-img(xn+ii-1,j,1))>d;
+                b2=abs(img(xn+ii,j,2)-img(xn+ii-1,j,2))>d;
+                b3=abs(img(xn+ii,j,3)-img(xn+ii-1,j,3))>d;
+                if(a1&&a2&&a3&&b1&&b2&&b3&&flagxn==1)
+                    flagxn=2;
+                    xn=xn-1;
+                elseif(a1&&a2&&a3&&flagxn==2)
+                    xn=xn-1;
+                else
+                    flagxn=0;
+                end
+            else
+                flagxn=0;
+            end
+            
+            if(yn+jj>1&&flagyn)
+                a1=abs(abs(img(i,yn+jj,1)-img(i,yn+jj-1,1))-abs(img(i,yn+jj,2)-img(i,yn+jj-1,2)))<s;
+                a2=abs(abs(img(i,yn+jj,1)-img(i,yn+jj-1,1))-abs(img(i,yn+jj,3)-img(i,yn+jj-1,3)))<s;
+                a3=abs(abs(img(i,yn+jj,3)-img(i,yn+jj-1,3))-abs(img(i,yn+jj,2)-img(i,yn+jj-1,2)))<s;
+                b1=abs(img(i,yn+jj,1)-img(i,yn+jj-1,1))>d;
+                b2=abs(img(i,yn+jj,2)-img(i,yn+jj-1,2))>d;
+                b3=abs(img(i,yn+jj,3)-img(i,yn+jj-1,3))>d;
+                if(a1&&a2&&a3&&b1&&b2&&b3&&flagyn==1)
+                    flagyn=2;
+                    yn=yn-1;
+                elseif(a1&&a2&&a3&&flagyn==2)
+                    yn=yn-1;
+                else
+                    flagyn=0;
+                end
+            else
+                flagyn=0;
+            end
+            
+            
+        end
+        imgy(i,j)=imgy(i,j)+yp;
+        imgx(i,j)=imgx(i,j)+yp;
+        imgx(ii,j)=imgx(ii,j)+xn;
+        imgy(i,jj)=imgy(i,jj)+yn;
+        jj=jj-1;
+    end
+    ii=ii-1;
+end
+imga=zeros([size,size]);
+%calculating angle of shadows
+disp("2");
+for i=1:size
+    for j=1:size
+        if(abs(imgy(i,j))~=0)
+            imga(i,j)=180*atan(abs(imgx(i,j))/abs(imgy(i,j)))/pi;
+            if(imgy(i,j)>0)
+                imga(i,j)=imga(i,j)+0.001;
+            end
+        elseif(abs(imgx(i,j))>0)
+            imga(i,j)=90;
+        end
+    end
+end
+% calculating number of shadows facing the same direction
+imgcn=zeros([size,size]);
+disp("3");
+for i=1:size
+    for j=i:size
+        disp(i)
+        disp(j)
+        if(imga(i,j)>0)
+            for x=1:size
+                for y=1:size
+                    if(abs(img(i,j)-img(x,y))<10&&imgx(i,j)*imgx(x,y)>0&&imgy(i,j)*imgy(x,y)>0)
+                        imgcn(x,y)=imgcn(x,y)+1;
+                        imgcn(i,j)=imgcn(i,j)+1;
+                    end
+                end
             end
         end
     end
 end
-%Getting watermark from image
-imgout=zeros([maskSize,maskSize]);
-for i = 1:imgSize
-    for j = 1:imgSize
-        m=mod(i,maskSize)+1;
-        n=mod(j,maskSize)+1;
-        b=key(mod((i*imgSize)+j,length(key))+1);
-        if(imgPossible(i,j,b)==1)
-            imgout(m,n)=imgout(m,n)+extractBit(img,b,[i,j]);
+%Higlighting the ponts of interest
+disp("4");
+imgcp=img;
+for i=1:size
+    for j=1:size
+        disp(i)
+        disp(j)
+        if(imgcn(i,j)==min(imgcn(imgcn > 0)))
+            imgcp(i,j,:)=[255 0 0];
+            for w=i-5:i+5
+                for h=j-5:j+5
+                    if(w>0&&w<size&&h>0&&h<size)
+                        imgcp(w,h,:)=[255 0 0];
+                    end
+                end
+            end
         end
     end
 end
-imgwater=imgout;
-img2=img;
-%Compressing as JPEG
-imwrite(img,"imJPG.jpg");
-img=imread("imJPG.jpg");
-% Getting watermark from JPEG image
-imgout=zeros([maskSize,maskSize]);
-for i = 1:imgSize
-    for j = 1:imgSize
-        m=mod(i,maskSize)+1;
-        n=mod(j,maskSize)+1;
-        b=key(mod((i*imgSize)+j,length(key))+1);
-        if(imgPossible(i,j,b)==1)
-            imgout(m,n)=imgout(m,n)+extractBit(img,b,[i,j]);
-        end
-    end
-end
-imgJPG=imgout;
-%Plotting images
-subplot(1,4,1)
-imshow(imgin,[])
-subplot(1,4,2)
-imshow(img,[])
-subplot(1,4,3)
-imshow(imgwater,[])
-subplot(1,4,4)
-imshow(imgJPG,[])
- 
-%PSNR Calculation
-k=sum(double(img2-imgin).^2,'all')/(imgSize*imgSize);
-s=double(255);
-disp(20*log(s/k))
- 
-%For adding to bit location
-function img = addToBit(img,bit,pos,val)
-    imgb=extractBit(img,bit,pos);
-    if(imgb~=val)
-        if(val>imgb)
-            img(pos(1),pos(2))=img(pos(1),pos(2))+(2^bit);
-        else
-            img(pos(1),pos(2))=img(pos(1),pos(2))-(2^bit);
-        end
-    end
-end
-%For extracting bit from location
-function val = extractBit(img,bit,pos)
-    imgb=double(img(pos(1),pos(2)))/(2^bit);
-    imgb=fix(mod(imgb,2));
-    val=imgb;
-end
- 
- 
- 
- 
- 
+%outputing the results
+subplot(1,6,1);
+imshow(img);
+subplot(1,6,2);
+imshow(imgx,[]);
+subplot(1,6,3);
+imshow(imgy,[]);
+subplot(1,6,4);
+imshow(imga,[]);
+subplot(1,6,5);
+imshow(imgcn,[]);
+subplot(1,6,6);
+imshow(imgcp);
  
